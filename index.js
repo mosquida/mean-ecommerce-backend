77ce44bace89d195dd0db2361116015ccdea9014
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2022-10-03 10:35:59 +0800

    Add and Retrieve Data via Mongoose Model

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 // Middlewares
 app.use(express.json()); // JSON parser
 app.use(morgan("tiny")); // HTTP logger
 
-app.get("/", (req, res) => {
-  const product = {
-    id: 1,
-    name: "Intel CPU",
-    price: 200,
-  };
-  res.json(product);
+// Collection Schema
+const productSchema = mongoose.Schema({
+  name: String,
+  price: Number,
+});
+// Model from schema
+const Product = mongoose.model("Product", productSchema);
+
+// USING ASYNC AWAIT
+app.get("/", async (req, res) => {
+  // Get all products
+  const products = await Product.find();
+  return res.status(200).json(products);
 });
 
+// USING THEN CATCH
 app.post("/", (req, res) => {
-  res.json(req.body);
+  // Create new item at product collection
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+  });
+  // Save to database
+  product
+    .save()
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err));
+
+  return;
 });
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Database Connected ...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Running on port ${process.env.PORT}`);
+  console.log(`API running on port ${process.env.PORT}`);
 });
