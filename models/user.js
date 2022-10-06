e19a449f864c7b216a361b5e20da1f0a179c648e
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2022-10-06 10:41:53 +0800

    Added Mongoose Password Hashing

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
@@ -12,5 +13,35 @@ const userSchema = mongoose.Schema({
   country: { type: String, default: "" },
   isAdmin: { type: Boolean, default: false },
 });
+
+// Hash the password before saving
+userSchema.pre("save", function (next) {
+  // this, refers to the model itself
+  let user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
+
+  // Generate Salt
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    // Hash Password with salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Compare user password against hashed password
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model("User", userSchema);
