e9b663c60427101dc505053efc5db44dd32c9692
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2022-10-06 11:51:24 +0800

    Added Get User & Get UserList Route, password excluded

--- models/user.js
+++ models/user.js
@@ -40,6 +40,7 @@ userSchema.pre("save", function (next) {
 userSchema.methods.comparePassword = function (candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
+
     cb(null, isMatch);
   });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,30 @@
 const router = require("express").Router();
+const user = require("../models/user");
 const User = require("../models/user");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().select(["-password"]); //excludes passwords using "-"
+
+    if (!users) return res.status(404).json({ message: "No users found" });
+
+    return res.json(users);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select(["-password"]);
+
+    if (!user) return res.status(404).json({ message: "No user found" });
+
+    return res.json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 // Register New User
 router.post("/", async (req, res) => {
