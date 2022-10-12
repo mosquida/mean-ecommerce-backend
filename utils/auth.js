5be8da26b5f2e34b1a9d2d2528d8f70389467e0f
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2022-10-12 21:01:00 +0800

    Excluded some routes to jwt auth middleware"

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,20 @@
 var { expressjwt: jwt } = require("express-jwt");
 
+// prettier-ignore
 module.exports = function () {
   return jwt({
     secret: process.env.JWT_SECRET_KEY,
     algorithms: ["HS256"],
+  }).unless({
+    path: [
+      "/api/v1/users/login",
+      "/api/v1/users/register",
+      { url: /^\/api\/v1\/products\/.*/, methods: ["GET"] },
+      { url: /^\/api\/v1\/categories\/.*/, methods: ["GET"] },
+    ],
   });
+  // Exlude routes to jwt auth middleware using .unless()
+  // Regex can be used in url path
+
+  //"/\api/\v1/\products(.*)" = all subroutes of products, ex products/1
 };
